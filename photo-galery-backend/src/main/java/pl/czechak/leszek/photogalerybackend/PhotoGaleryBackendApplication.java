fc7785f5769b22a66258ef8c1a0f0423f71cc523
Leszek Czechak <leszekczechak@gmail.com>
2020-12-08 15:04:09 +0100

    Initial commit

---
+++ photo-galery-backend/src/main/java/pl/czechak/leszek/photogalerybackend/PhotoGaleryBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.czechak.leszek.photogalerybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoGaleryBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoGaleryBackendApplication.class, args);
+    }
+
+}
---
+++ photo-galery-backend/src/test/java/pl/czechak/leszek/photogalerybackend/PhotoGaleryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.czechak.leszek.photogalerybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoGaleryBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
