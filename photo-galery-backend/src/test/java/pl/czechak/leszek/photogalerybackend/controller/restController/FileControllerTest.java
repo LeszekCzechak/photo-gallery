2e21bbbb4f8af25f82f63ef59ab358fdad7bc031
Leszek Czechak <leszekczechak@gmail.com>
2020-12-10 17:39:34 +0100

    FileControllerTest fixed UserServiceTest started partly tested

--- photo-galery-backend/src/test/java/pl/czechak/leszek/photogalerybackend/controller/restController/FileControllerTest.java
+++ photo-galery-backend/src/test/java/pl/czechak/leszek/photogalerybackend/controller/restController/FileControllerTest.java
@@ -2,69 +2,63 @@ package pl.czechak.leszek.photogalerybackend.controller.restController;
 
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.web.multipart.MultipartFile;
+import pl.czechak.leszek.photogalerybackend.configuration.security.EncryptorConfiguration;
+import pl.czechak.leszek.photogalerybackend.configuration.security.SecurityConfiguration;
+import pl.czechak.leszek.photogalerybackend.repository.FileRepository;
 import pl.czechak.leszek.photogalerybackend.service.FileService;
 import pl.czechak.leszek.photogalerybackend.service.UserService;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(FileController.class)
+@WebMvcTest(value = FileController.class, excludeAutoConfiguration = {SecurityConfiguration.class, EncryptorConfiguration.class})
 class FileControllerTest {
-
     @Autowired
     private MockMvc mockMvc;
-
     @MockBean
     private FileService fileService;
     @MockBean
-    private FileController fileController;
+    private FileRepository fileRepository;
     @MockBean
     private UserService userService;
-
     @Test
+    @WithMockUser(username = "user", roles = "ADMIN")
     void shouldPassMultipartFileToFIleService() throws Exception {
-
-        MockMultipartFile multipartFile
-                = new MockMultipartFile(
+        MockMultipartFile multipartFile = new MockMultipartFile(
                 "multipartFile",
                 "hello.txt",
                 MediaType.TEXT_PLAIN_VALUE,
                 "Hello, World!".getBytes()
         );
-
-        mockMvc.perform(post("/files/1")
-                .contentType(MediaType.MULTIPART_FORM_DATA)
-                .content(String.valueOf(multipartFile)))
+        mockMvc.perform(MockMvcRequestBuilders.multipart("/files/1")
+                .file(multipartFile))
                 .andDo((print()))
                 .andExpect(status().isOk());
-
         ArgumentCaptor<MultipartFile> captor = ArgumentCaptor.forClass(MultipartFile.class);
-        verify(fileService).addFileToGallery(1, captor.capture());
-
-        assertEquals("multipartFile",captor.capture().getName());
-        assertEquals("hello.txt",captor.capture().getOriginalFilename());
-
+        verify(fileService).addFileToGallery(eq(1L), captor.capture());
+        assertEquals("multipartFile", captor.getValue().getName());
+        assertEquals("hello.txt", captor.getValue().getOriginalFilename());
     }
 
     @Test
+    @WithMockUser(username = "user", roles = "ADMIN")
     void shouldDeleteFile() throws Exception {
-
-        mockMvc.perform(delete("/1"))
+        mockMvc.perform(delete("/files/1"))
                 .andExpect(status().isOk());
+        verify(fileService).deleteFile(1);
     }
 }
---
+++ photo-galery-backend/src/test/java/pl/czechak/leszek/photogalerybackend/service/UserServiceTest.java
@@ -0,0 +1,140 @@
+package pl.czechak.leszek.photogalerybackend.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import pl.czechak.leszek.photogalerybackend.dto.CreateUserRequest;
+import pl.czechak.leszek.photogalerybackend.model.user.UserEntity;
+import pl.czechak.leszek.photogalerybackend.model.user.UserRole;
+import pl.czechak.leszek.photogalerybackend.repository.GalleryRepository;
+import pl.czechak.leszek.photogalerybackend.repository.UserRepository;
+
+import java.util.Optional;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+    @Mock
+    private GalleryRepository galleryRepository;
+    @Mock
+    private BCryptPasswordEncoder passwordEncoder;
+
+    private UserService userService;
+
+    @BeforeEach
+    void setUp() {
+        userService = new UserService(galleryRepository, userRepository, passwordEncoder);
+    }
+
+
+    @Test
+    void shouldReturnUserDetailsByUsername() {
+        //given
+        String username = "UserTest";
+        String password = "password";
+        UserEntity userEntity = new UserEntity(1L, "UserTest", password, Set.of(UserRole.USER, UserRole.ADMIN), null);
+
+        Optional<UserEntity> optionalUserEntity = Optional.of(userEntity);
+
+        when(userRepository.findUserEntityByUsername(username)).thenReturn(Optional.of(userEntity));
+
+        //when
+        UserDetails userDetails = userService.loadUserByUsername(username);
+        //then
+        assertThat(userDetails.getUsername()).isEqualTo(username);
+        assertThat(userDetails.getPassword()).isEqualTo(password);
+    }
+
+    @Test
+    void shouldCreateUserWithAdminRole() {
+        //given
+        CreateUserRequest userRequest = new CreateUserRequest();
+
+        String username = "UserTest";
+        String password = "password";
+
+        userRequest.setUsername(username);
+        userRequest.setPassword(password);
+        userRequest.setAdmin(true);
+        //when
+        userService.createUser(userRequest);
+        //then
+        ArgumentCaptor<UserEntity> argumentCaptor = ArgumentCaptor.forClass(UserEntity.class);
+        Mockito.verify(userRepository).save(argumentCaptor.capture());
+        UserEntity entity = argumentCaptor.getValue();
+        assertThat(entity.getUsername()).isEqualTo(username);
+        assertThat(entity.getRoles()).contains(UserRole.ADMIN);
+        assertThat(entity.getRoles()).contains(UserRole.USER);
+    }
+
+    @Test
+    void shouldCreateUserWithoutAdminRole() {
+        //given
+        CreateUserRequest userRequest = new CreateUserRequest();
+
+        String username = "UserTest";
+        String password = "password";
+
+        userRequest.setUsername(username);
+        userRequest.setPassword(password);
+        userRequest.setAdmin(false);
+        //when
+        userService.createUser(userRequest);
+        //then
+        ArgumentCaptor<UserEntity> argumentCaptor = ArgumentCaptor.forClass(UserEntity.class);
+        Mockito.verify(userRepository).save(argumentCaptor.capture());
+        UserEntity entity = argumentCaptor.getValue();
+        assertThat(entity.getUsername()).isEqualTo(username);
+        assertThat(entity.getRoles()).containsOnly(UserRole.USER);
+        assertThat(entity.getRoles()).doesNotContain(UserRole.ADMIN);
+    }
+
+    @Test
+    void shouldDeleteUser() {
+
+        // given
+        Long userId = 999L;
+        String username = "UserTest";
+        UserEntity userEntity = new UserEntity(userId, username, "password", Set.of(UserRole.USER, UserRole.ADMIN), null);
+        Optional<UserEntity> optionalUserEntity = Optional.of(userEntity);
+
+        when(userRepository.findById(userId)).thenReturn(optionalUserEntity);
+
+        //when
+        userService.deleteUser(userId);
+
+        //then
+        ArgumentCaptor<UserEntity> argumentCaptor= ArgumentCaptor.forClass(UserEntity.class);
+        Mockito.verify(userRepository).delete(argumentCaptor.capture());
+        UserEntity entity = argumentCaptor.getValue();
+        assertThat(entity.getUsername()).isEqualTo(username);
+        assertThat(entity.getId()).isEqualTo(userId);
+
+    }
+
+    @Test
+    void shouldReturnListOfUserResponse() {
+
+        //given
+
+        //when
+        //then
+
+    }
+
+    @Test
+    void checkLoginStatus() {
+    }
+}
